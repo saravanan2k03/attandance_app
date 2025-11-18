932c23696dbf25d8c1158f8c004661dd76b91717
saravananmam <saravananm@am.amrita.edu>
2025-11-18 15:51:53 +0530

    Final Docker Setup

--- act/settings.py
+++ act/settings.py
@@ -140,7 +140,7 @@ DATABASES = {
         "NAME":  os.getenv("DBNAME"),
         "USER": os.getenv("DBUSER"),
         "PASSWORD": os.getenv("DBPASSWORD"),
-        "HOST": "127.0.0.1",
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"), 
         "PORT": "3306",
         "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Waiting for MySQL..."
+while ! nc -z db 3306; do
+  sleep 0.1
+done
+echo "MySQL started"
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+exec "$@"
